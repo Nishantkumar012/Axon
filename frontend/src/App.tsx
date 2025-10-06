18e9d94fcdef95464d69f7fbd041ca205f50f459
Nishantkumar012 <124617537+Nishantkumar012@users.noreply.github.com>
2025-10-06 10:46:12 +0530

    Add login form validation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,9 @@
 import './App.css'
 import Home from "./pages/landingpage/AxionLanding"
 import Signup from "./pages/authPages/Signup.tsx"
+import Login from "./pages/authPages/Login.tsx"
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+// import { LogIn } from 'lucide-react'
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path='/login' element={<Login/>}/>
         </Routes>
       </BrowserRouter>
     </>
