1038a87c67abf16d8cc889390669568fa1b20d7c
Nishantkumar012 <124617537+Nishantkumar012@users.noreply.github.com>
2025-10-06 10:19:04 +0530

    Add Signup feature

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,20 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import './App.css'
-import Home from "./AxionLanding"
-
+import Home from "./pages/landingpage/AxionLanding"
+import Signup from "./pages/authPages/Signup.tsx"
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
     <>
-      
-      <Home/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
 
 export default App
+
